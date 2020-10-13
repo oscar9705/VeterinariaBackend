c339442b71cd69beaa08f5960c4654322293d808
oscar salazar <salazaroscar9705@gmail.com>
2020-10-13 15:20:20 -0500

    primer test- service Test1.1

--- src/test/java/com/practica/proyecto/service/VeterinariaServiceTest.java
+++ src/test/java/com/practica/proyecto/service/VeterinariaServiceTest.java
@@ -2,6 +2,8 @@ package com.practica.proyecto.service;
 
 import com.practica.proyecto.model.Sucursal;
 import com.practica.proyecto.model.Veterinaria;
+import com.practica.proyecto.model.VeterinariaDTO;
+import com.practica.proyecto.repository.SucursalRepository;
 import com.practica.proyecto.repository.VeterinariaRepository;
 
 
@@ -24,6 +26,11 @@ import java.util.List;
 import java.util.Optional;
 
 public class VeterinariaServiceTest {
+    private static final Long ID_SUC = Long.valueOf(1);
+    private static final String DIRECCION = "CRA 80";
+    private static final String TELEFONO = "4454341";
+    private static final String IMAGEN = "foto.jpg";
+
 
     private static final Long ID = Long.valueOf(1);
     private static final int ID_DUENIO = 1;
@@ -33,10 +40,17 @@ public class VeterinariaServiceTest {
     private static final Boolean ESTADO = true;
     private static List<Sucursal> SUCURSAL_LIST = new ArrayList<>();
     private static final Veterinaria VETERINARIA = new Veterinaria();
+    private static final Sucursal SUCURSAL = new Sucursal();
+    private static final VeterinariaDTO VETERINARIA_DTO = new VeterinariaDTO();
     private static final Optional<Veterinaria> OPTIONAL_VETERINARIA = Optional.of(VETERINARIA);
+    private static final Optional<Sucursal> OPTIONAL_SUCURSAL = Optional.of(SUCURSAL);
+    private static final Optional<VeterinariaDTO> OPTIONAL_VETERINARIA_DTO = Optional.of(VETERINARIA_DTO);
 
     @Mock
     private VeterinariaRepository veterinariaRepository;
+
+    @Mock
+    private SucursalRepository sucursalRepository;
     @InjectMocks
     VeterinariaService veterinariaService;
     @BeforeEach
@@ -49,16 +63,31 @@ public class VeterinariaServiceTest {
         VETERINARIA.setDuenio(ID_DUENIO);
         VETERINARIA.setSucursalList(SUCURSAL_LIST);
 
+        VETERINARIA_DTO.setNombreVet(NOMBRE);
+        VETERINARIA_DTO.setNit(NIT);
+        VETERINARIA_DTO.setCorreo(CORREO);
+        VETERINARIA_DTO.setTelefono(TELEFONO);
+        VETERINARIA_DTO.setImagen(IMAGEN);
+
+        SUCURSAL.setDireccion(DIRECCION);
+        SUCURSAL.setImagen(IMAGEN);
+        SUCURSAL.setTelefono(TELEFONO);
+        SUCURSAL.setVeterinaria(VETERINARIA);
+
+
+
+
+
 
     }
     @Test
-    public void findByIdTest(){
+    void findByIdTest(){
         Mockito.when(veterinariaRepository.findById(ID)).thenReturn(OPTIONAL_VETERINARIA);
         veterinariaService.findById(ID);
 
     }
     @Test
-    public void findByAllTest(){
+    void findByAllTest(){
         final Veterinaria veterinaria = new Veterinaria();
         Mockito.when(veterinariaRepository.findAll()).thenReturn(Arrays.asList(veterinaria));
         final List<Veterinaria> resp = veterinariaService.findAll();
@@ -66,6 +95,14 @@ public class VeterinariaServiceTest {
         assertFalse(resp.isEmpty());
         assertEquals(resp.size(), 1);
 
+    }
+
+    @Test
+    void findbyIdVetSucTest(){
+        Mockito.when(veterinariaRepository.findById(ID)).thenReturn(OPTIONAL_VETERINARIA);
+        Mockito.when(sucursalRepository.findById(ID_SUC)).thenReturn(OPTIONAL_SUCURSAL);
+        Mockito.when(veterinariaService.findbyIdVetSuc(ID,ID_SUC)).thenReturn(OPTIONAL_VETERINARIA_DTO);
+        veterinariaService.findbyIdVetSuc(ID,ID_SUC);
     }
 
 
